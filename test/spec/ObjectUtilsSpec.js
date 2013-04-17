18cd549931c70f2c2629a8dddacd5b830b844111
Craig Cook <boycook@me.com>
2013-04-17 17:11:23 +0100

    Added more tests

--- test/spec/ObjectUtilsSpec.js
+++ test/spec/ObjectUtilsSpec.js
@@ -3,15 +3,86 @@ var Object = require('../../lib/objectutils');
 
 describe('ObjectUtils', function () {
 
-    it('should think defined is defined', function () {
-        Object.isDefined({}).should.be.true;
+    describe('#isDefined', function () {
+        it('should think defined is defined', function () {
+            Object.isDefined({}).should.be.true;
+        });
+
+        it('should not think undefined is defined', function () {
+            Object.isDefined(undefined).should.be.false;
+        });
     });
 
-    it('should not think undefined is defined', function () {
-        Object.isDefined(undefined).should.be.false;
+    describe('#isUndefined', function () {
+        it('should think undefined is undefined', function () {
+            Object.isUndefined(undefined).should.be.true;
+        });
+
+        it('should think empty is undefined', function () {
+            Object.isUndefined().should.be.true;
+        });
+
+        it('should think not object is undefined', function () {
+            Object.isUndefined({}).should.be.false;
+        });
     });
 
-    it('should think undefined is undefined', function () {
-        Object.isUndefined(undefined).should.be.true;
+
+    describe('#isEmpty', function () {
+        it('should think no value is empty', function () {
+            Object.isEmpty().should.be.true;
+        });
+
+        it('should think empty array is empty', function () {
+            Object.isEmpty([]).should.be.true;
+        });
+
+        it('should think empty string is empty', function () {
+            Object.isEmpty('').should.be.true;
+        });
+
+        it('should not think array with item is empty', function () {
+            Object.isEmpty([1]).should.be.false;
+        });
+
+        it('should not think string is empty', function () {
+            Object.isEmpty('value').should.be.false;
+        });
+    });
+
+    describe('#isNotEmpty', function () {
+        it('should thinkg value is not empty', function () {
+            Object.isNotEmpty('value').should.be.true;
+        });
+
+        it('should think array with value is not empty', function () {
+            Object.isNotEmpty([1]).should.be.true;
+        });
+
+        it('should not think empty string is not empty', function () {
+            Object.isNotEmpty('').should.be.false;
+        });
+
+        it('should not think empty array is not empty', function () {
+            Object.isNotEmpty([]).should.be.false;
+        });
+    });
+
+    describe('#isObject', function () {
+        it('should think object is an object', function () {
+            Object.isObject({}).should.be.true;
+        });
+
+        it('should not think string is an object', function () {
+            Object.isObject('').should.be.false;
+        });
+
+        it('should not think int is an object', function () {
+            Object.isObject(1).should.be.false;
+        });
+    });
+
+    describe('#isNotObject', function () {
+
     });
 });
